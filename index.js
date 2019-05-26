4f49f48d2de172de62a40f620cabe8a4ed67ed34
wen <bongiovi015@gmail.com>
2019-05-26 11:55:43 +0100

    update randomGaussian functino

--- index.js
+++ index.js
@@ -1,65 +1,39 @@
 // index.js
 
-var defaultTo = function(value, defaultValue) {
-    return isNaN(value) ? defaultValue : value;
-}
-
-Math.randomGaussian = function(mean, standardDeviation) {
-
-    standardDeviation = defaultTo(standardDeviation, 0.25);
-
-    if (Math.randomGaussian.nextGaussian !== undefined) {
-        var nextGaussian = Math.randomGaussian.nextGaussian;
-        delete Math.randomGaussian.nextGaussian;
-        return (nextGaussian * standardDeviation) + mean;
-    } else {
-        var v1, v2, s, multiplier;
-        do {
-            v1 = 2 * Math.random() - 1; // between -1 and 1
-            v2 = 2 * Math.random() - 1; // between -1 and 1
-            s = v1 * v1 + v2 * v2;
-        } while (s >= 1 || s == 0);
-        multiplier = Math.sqrt(-2 * Math.log(s) / s);
-        Math.randomGaussian.nextGaussian = v2 * multiplier;
-        return (v1 * multiplier * standardDeviation) + mean;
-    }
-
-};
-
-
 var random = function(a, b) {
-	if(b === undefined) {
-		b = 0;
-	}
-	return a + Math.random() * (b - a);
+    if(b === undefined) {
+        b = 0;
+    }
+    return a + Math.random() * (b - a);
 }
 
 
 var randomFloor = function(a, b) {
-	return Math.floor(random(a, b));
+    return Math.floor(random(a, b));
 }
 
 
-var randomGaussian = function() {
-	let rnd;
-
-	do {
-		rnd = Math.randomGaussian(0.5, 0.5);
-	} while(rnd < 0 || rnd > 1);
-
-	return rnd;
+var randomGaussian = function(n) {
+    if( n === undefined) n = 6;
+    var rand = 0;
+  
+    for (var i = 0; i < n; i += 1) {
+        rand += Math.random();
+    }
+  
+    return rand / n
 }
 
 var getRandomElement = function(ary) {
-	return ary[randomFloor(ary.length)];
+    return ary[randomFloor(ary.length)];
 }
 
 
 var randomUtils = {
-	random,
-	randomFloor,
-	randomGaussian,
-	getRandomElement
+    random,
+    randomFloor,
+    randomGaussian,
+    getRandomElement
 }
 
 
